7c639673ea2f3b8047b2e6aa6f9fe968d60f5bd9
kirua <yo44prg@icloud.com>
2022-03-01 23:37:45 +0100

    feat: add parallax and section models

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,26 @@
 <template>
-  <v-container>
+  <div>
+    <v-parallax
+      dark
+      src="../assets/model-test.jpg"
+    >
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col
+          class="text-center"
+          cols="12"
+        >
+          <h1 class="text-h4 font-weight-thin mb-4">
+            AG Scouting
+          </h1>
+          <h4 class="subheading">
+            Phrase d'accroche
+          </h4>
+        </v-col>
+      </v-row>
+    </v-parallax>
     <v-row class="text-center">
       <v-col cols="12">
         <v-img
@@ -88,64 +109,70 @@
         </v-row>
       </v-col>
     </v-row>
-  </v-container>
+    <ModelSection/>
+    <div style="min-height:100vh"></div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'HomePage',
+import ModelSection from "./section/ModelSection.vue";
 
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+export default {
+  name: "HomePage",
+  components: {
+    ModelSection
+  },
+  data: () => ({
+    ecosystem: [
+      {
+        text: 'vuetify-loader',
+        href: 'https://github.com/vuetifyjs/vuetify-loader',
+      },
+      {
+        text: 'github',
+        href: 'https://github.com/vuetifyjs/vuetify',
+      },
+      {
+        text: 'awesome-vuetify',
+        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+      },
+    ],
+    importantLinks: [
+      {
+        text: 'Documentation',
+        href: 'https://vuetifyjs.com',
+      },
+      {
+        text: 'Chat',
+        href: 'https://community.vuetifyjs.com',
+      },
+      {
+        text: 'Made with Vuetify',
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer',
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
+}
 </script>
---
+++ src/components/section/ModelSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="model-section">
+      <h2>Model section</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "ModelSection"
+}
+</script>
+
+<style>
+
+</style>
