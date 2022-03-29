ba5e414020ca4f4a0e5b56b0b46907bb7a7e893a
kirua <yo44prg@icloud.com>
2022-03-30 01:48:48 +0200

    feat: add page all models for administration

---
+++ src/components/AdminModel.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ModelSection/>
+  </div>
+</template>
+
+<script>
+import ModelSection from "./section/ModelSection.vue";
+
+
+export default {
+  components: {
+    ModelSection
+  }
+}
+</script>
+
+<style>
+
+</style>
