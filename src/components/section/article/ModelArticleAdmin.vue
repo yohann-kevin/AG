c0bb0557dda738ce13dbd43f73bcaae87e9c8472
kirua <yo44prg@icloud.com>
2022-04-10 17:09:44 +0200

    feat: add page adminModel

---
+++ src/components/section/article/ModelArticleAdmin.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card
+    class="model-article-admin"
+    max-width="344"
+    elevation="2"
+  >
+    <v-img
+      :src="model.model_picture.picture_path"
+      height="300px"
+    ></v-img>
+
+    <v-card-title>
+      {{ model.model.firstname }}
+    </v-card-title>
+
+    <v-card-subtitle>
+      {{ model.model_info.description }}
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn color="black lighten-2" text>
+        Voir plus
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  }),
+  props: {
+    model: {
+      type: Object,
+      required: true
+    },
+  },
+  methods: {
+    // redirectToModel(modelId) {
+    //   this.$store.commit("modelId", modelId);
+    //   this.$router.push({ path: "/model" });
+    // }
+  }
+}
+</script>
+
+<style scoped>
+.model-article-admin {
+  margin-bottom: 25px;
+  margin-top: 25px;
+}
+
+.more-btn {
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.87);
+}
+</style>
