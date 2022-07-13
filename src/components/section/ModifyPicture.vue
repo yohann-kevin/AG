4995b305c6fbe6df20fdcaa4d84c77664c9475ea
kirua <yo44prg@icloud.com>
2022-07-14 01:14:38 +0200

    feat(ModifyPicture): add model picture deletion [AGS-18]

--- src/components/section/ModifyPicture.vue
+++ src/components/section/ModifyPicture.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="modify-picture-section">
     <h2 class="mb-3">Modifier les images du modèles</h2>
-    <v-card v-for="(picture, i) in pictures" :key="i" class="image-card">
+    <v-card v-for="(picture, i) in modelPictures" :key="i" class="image-card">
       <v-img
         :src="picture.picture_path"
         height="250px"
@@ -25,25 +25,94 @@
         dark
         small
         color="error"
+        @click="openModalDeletePicture(picture.id)"
       >
         <v-icon dark>
           mdi-trash-can-outline
         </v-icon>
       </v-btn>
     </v-card>
+
+    <div>
+      <ModalDeletePicture v-model="showModal" v-on:accept="deletePicture"/>
+    </div>
+
+    <div class="delete-picture-alert">
+      <v-alert
+        elevation="15"
+        dense
+        text
+        dismissible
+        type="error"
+        v-model="errorAlert"
+      >
+        La suppression de la photo n'a pas fonctionner !
+      </v-alert>
+
+      <v-alert
+        elevation="15"
+        dense
+        text
+        dismissible
+        type="success"
+        v-model="successAlert"
+      >
+        La photo a bien été supprimer !
+      </v-alert>
+    </div>
   </div>
 </template>
 
 <script>
+import ModalDeletePicture from '../modal/ModalDeletePicture'
+
 export default {
+  components: {
+    ModalDeletePicture,
+  },
+  data: () => ({
+    modelPictures: null,
+    showModal: false,
+    selectedPictureId: null,
+    errorAlert: false,
+    successAlert: false,
+  }),
   props: {
     pictures: {
-      type: Object,
+      type: Array,
       required: true
     }
   },
-  mounted() {
-    console.log(this.pictures);
+  beforeMount() {
+    this.modelPictures = this.pictures;
+  },
+  methods: {
+    openModalDeletePicture(pictureId) {
+      this.showModal = true;
+      this.selectedPictureId = pictureId;
+    },
+    deletePicture() {
+      const config = {
+        method: 'delete',
+        url: process.env.VUE_APP_API_URL +  '/model_pictures/' + this.selectedPictureId,
+        headers: { }
+      };
+
+      this.$axios(config).then(response => {
+        if (response.status === 204) {
+          this.successAlert = true;
+          this.removeImageInUi(this.selectedPictureId);
+          this.selectedPictureId = null;
+        }
+      }).catch(error => {
+        this.errorAlert = true;
+        console.log(error);
+      });
+    },
+    removeImageInUi(pictureId) {
+      const pictureDeleteIndex = this.modelPictures.findIndex(picture => picture.id === pictureId);
+      this.modelPictures.splice(pictureDeleteIndex, 1);
+    }
   }
 }
 </script>
@@ -63,5 +132,9 @@ export default {
 
 .gold-star {
   color: #FFD700;
+}
+
+.delete-picture-alert {
+  width: 80%;
 }
 </style>
