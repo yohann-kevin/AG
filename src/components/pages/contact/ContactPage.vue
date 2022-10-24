95bab0c8301170feb86cab284394049a2df82daf
kirua <yo44prg@icloud.com>
2022-10-24 21:29:41 +0200

    feat(ContactPage): rework page contact [AGS-98]

--- src/components/pages/contact/ContactPage.vue
+++ src/components/pages/contact/ContactPage.vue
@@ -7,31 +7,6 @@
       les plus brefs délais.
     </p>
 
-    <div class="contact-container-alert">
-      <v-alert
-        dense
-        text
-        dismissible
-        elevation="15"
-        type="error"
-        v-model="errorAlert"
-        class="contact-alert"
-      >
-        Quelque chose c'est mal passer réessayer de nous contactez plus tard !
-      </v-alert>
-      <v-alert
-        dense
-        text
-        dismissible
-        elevation="15"
-        type="success"
-        v-model="successAlert"
-        class="contact-alert"
-      >
-        Votre demande de contact à bien été envoyer !
-      </v-alert>
-    </div>
-
     <v-card
       elevation="8"
       class="contact-card"
@@ -39,7 +14,6 @@
       <v-form
         class="contact-form"
         ref="contact-form"
-        v-model="valid"
       >
         <v-text-field
           v-model="email"
@@ -48,7 +22,33 @@
           type="email"
           label="Courriel"
           class="contact-input"
+          prepend-icon="mdi-email"
           required
+        />
+
+        <v-text-field
+          v-model="phoneNumber"
+          label="Votre numéro de téléphone"
+          :rules="phoneRules"
+          color="black"
+          class="contact-input"
+          prepend-icon="mdi-phone"
+        />
+
+        <v-text-field
+          v-model="companyName"
+          color="black"
+          label="Nom de votre entreprise"
+          class="contact-input"
+          prepend-icon="mdi-home-city"
+        />
+
+        <v-text-field
+          v-model="activity"
+          color="black"
+          label="Votre secteur d'activité"
+          class="contact-input"
+          prepend-icon="mdi-briefcase"
         />
 
         <v-text-field
@@ -57,6 +57,20 @@
           label="Objet de votre demande"
           class="contact-input"
           required
+          prepend-icon="mdi-email-outline"
+        />
+
+        <v-select
+          v-model="needSelected"
+          :items="needs"
+          label="Votre besoin"
+          color="black"
+          item-color="black"
+          class="contact-input"
+          multiple
+          chips
+          required
+          prepend-icon="mdi-magnify"
         />
 
         <v-textarea
@@ -66,6 +80,7 @@
           class="contact-input"
           filled
           required
+          prepend-icon="mdi-message"
         />
       </v-form>
 
@@ -85,31 +100,117 @@
         </v-btn>
       </div>
     </v-card>
+    <div class="contact-container-alert">
+      <v-alert
+        dense
+        text
+        dismissible
+        elevation="15"
+        type="error"
+        v-model="errorAlert"
+        class="contact-alert"
+      >
+        Quelque chose c'est mal passer réessayer de nous contactez plus tard !
+      </v-alert>
+      <v-alert
+        dense
+        text
+        dismissible
+        elevation="15"
+        type="error"
+        v-model="formAlert"
+        class="contact-alert"
+      >
+        Votre email, votre besoin, votre message et l'objet de celui-ci sont requis pour envoyer une demande de contact  !
+      </v-alert>
+      <v-alert
+        dense
+        text
+        dismissible
+        elevation="15"
+        type="success"
+        v-model="successAlert"
+        class="contact-alert"
+      >
+        Votre demande de contact à bien été envoyer !
+      </v-alert>
+
+      <v-progress-circular
+        :size="70"
+        color="black"
+        indeterminate
+        v-if="isInLoad"
+        class="contact-progress"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    valid: false,
+    valid: true,
     successAlert: false,
     errorAlert: false,
+    formAlert: false,
     email: "",
     emailRules: [
       (v) =>
         /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v) || "Adresse email invalide",
     ],
-    object: "",
-    content: ""
+    object: null,
+    content: null,
+    needs: [
+      {
+        text: "Mannequin",
+        value: "mannequin",
+      },
+      {
+        text: "Photographe",
+        value: "photographe",
+      },
+      {
+        text: 'Studio',
+        value: 'studio'
+      },
+      {
+        text: 'Autre',
+        value: 'autre'
+      }
+    ],
+    needSelected: null,
+    phoneNumber: null,
+    phoneRules: [
+      (v) =>
+        /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/.test(v) || "Numéro de téléphone invalide",
+    ],
+    companyName: null,
+    activity: null,
+    isInLoad: false
   }),
   methods: {
     resetForm() {
       this.$refs['contact-form'].reset();
     },
+    formatNeed() {
+      if (!this.needSelected) return;
+      return this.needSelected.join('/');
+    },
+    checkContactForm() {
+      if (this.email && this.object && this.needSelected && this.content) return true;
+      this.formAlert = true;
+      return false;
+    },
     sendEmail() {
+      if (!this.checkContactForm()) return;
+      this.isInLoad = true;
       const data = {
         sender: this.email,
+        // phone: this.phoneNumber,
+        // company: this.companyName,
+        // activity: this.activity,
         object: this.object,
+        // need: this.formatNeed(),
         content: this.content
       };
 
@@ -125,14 +226,15 @@ export default {
 
       this.$axios(config).then(response => {
         if(response.status === 201) {
+          this.isInLoad = false;
           this.successAlert = true;
           this.$refs['contact-form'].reset();
         }
       }).catch(error => {
+        this.isInLoad = false;
         this.errorAlert = true;
         console.log(error);
       });
-
     }
   }
 };
@@ -159,16 +261,20 @@ export default {
 }
 
 .contact-container-alert {
-  width: 60%;
+  width: 70%;
   margin: 15px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 }
 
-.contact-alert {
+.contact-alert,
+.contact-progress {
   width: 100%;
 }
 
 .contact-card {
-  width: 60%;
+  width: 70%;
   margin: 15px;
   padding: 20px;
 }
